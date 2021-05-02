f982645f219d1a2494f21026016b73b22609dcf4
Peter <peoriola@unionbankng.com>
2021-05-02 15:20:01 +0100

    Initial Commit

---
+++ PayoneerHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PayoneerHome
+//
+//  Created by UNION on 02/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
